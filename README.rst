57e1f19fdcca012dc3b7be27ae0b74b2ea8b0ec7
Daryl Walleck <daryl.walleck@rackspace.com>
2016-03-22 22:40:30 -0500

    Initial add

---
+++ README.rst
@@ -0,0 +1,8 @@
+subunit-parser
+==============
+
+Outputs all HTTP calls a given test made that were logged.
+
+::
+
+    subunit-describe-calls [-s/--subunit] [-n/--non-subunit-name] [-o/--output-file]
---
+++ process_results.py
@@ -0,0 +1,17 @@
+import json
+
+f = open('results.json')
+data = f.read()
+
+tests = json.loads(data)
+
+details = open('test_request_details.txt', 'w')
+for k, v in tests.iteritems():
+    test_name = k.split('[')[0]
+    
+    details.write(test_name + '\n')
+    
+    if v:
+        for item in v:
+            details.write('- ' + item.get('verb') + ' request for ' + item.get('service') + ' to ' + item.get('url') + '\n')
+    details.write('\n')
